046bd0d73317cfc3d7806c0cb9ae498d0004606e
SANTOS PEDRO BALTAZAR JOJ CANO <santospedrobaltazarjc02@gmail.com>
2023-06-06 17:45:23 -0600

    Decimosexto commit

--- src/components/_partials/Navbar.jsx
+++ src/components/_partials/Navbar.jsx
@@ -1,47 +1,47 @@
-import {Link} from 'react-router-dom'
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { Link } from 'react-router-dom'
+import ThemeSwitcher from './ThemeSwitcher'
+import IconLight from '../Icons/IconLight'
 
-function Navbar({darkMode, setDarkMode}) {
- 
-  return (  
-<nav className="bg-white rounded-lg shadow m-1  dark:bg-zinc-950 border-gray-200 ">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a className="flex items-center">
-      <img src="/icon2.svg" className="h-8 mr-3 dark:" alt="Flowbite Logo" />
-    </a>
-       <ul className="flex list-none m-0 p-0">
-        <li>
-        <Link className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500' to="/">Inicio</Link>
-        </li>
-        <li>
-        <Link className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' to="/catalogo">Catálogo</Link>
-        </li>
-        <li>
-        <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/contacto">Contacto</Link>
-        </li>
-        <li>
-        <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/sobre-nosotros">Sobre Nosotros</Link>
-        </li>
-        <li>
-        <Link className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/signin">Iniciar Sesión</Link>
-        </li>
-        <li>
-        <BsFillMoonStarsFill className="cursor-pointer" onClick={() => setDarkMode(!darkMode)}/>
-        </li>
-      </ul>
-  </div>
-</nav>
-    // <div>
-    // <header>
-    //     <nav>
-    //       <ul>
-    //       <BsFillMoonStarsFill classNameNameNameNameNameNameNameNameNameName="cursor-pointer" onClick={() => setDarkMode(!darkMode)}/>
-    //         <li><Link to="/">Home</Link></li>
-    //         <li><Link to="/search-page">Search Page</Link></li>
-    //       </ul>
-    //     </nav>
-    //   </header>
-    //   </div>
-  );
+function Navbar () {
+  return (
+    <nav className='bg-white rounded-lg shadow m-1 my-0 dark:bg-slate-800 border-gray-200'>
+      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <Link className='flex items-center' to='/'>
+          <IconLight fill='#000' />
+        </Link>
+        <ul className='flex list-none m-0 p-0'>
+          <li>
+            <Link className='block py-2 pl-3 pr-4 text-black rounded md:bg-transparent  md:p-0 dark:text-white focus:outline-none hover:bg-indigo-400 active:bg-indigo-600' to='/'>Inicio</Link>
+          </li>
+          <li>
+            <Link className='block py-2 pl-3 pr-4 text-black rounded md:bg-transparent  md:p-0 dark:text-white focus:outline-none hover:bg-indigo-400 active:bg-indigo-600' to='/catalogo'>Catálogo</Link>
+          </li>
+          <li>
+            <Link className='block py-2 pl-3 pr-4 text-black rounded md:bg-transparent  md:p-0 dark:text-white focus:outline-none hover:bg-indigo-400 active:bg-indigo-600' to='/contacto'>Contacto</Link>
+          </li>
+          <li>
+            <Link className='block py-2 pl-3 pr-4 text-black rounded md:bg-transparent  md:p-0 dark:text-white focus:outline-none hover:bg-indigo-400 active:bg-indigo-600' to='/sobre-nosotros'>Sobre Nosotros</Link>
+          </li>
+          <li>
+            <Link className='block py-2 pl-3 pr-4 text-black rounded md:bg-transparent  md:p-0 dark:text-white focus:outline-none hover:bg-indigo-400 active:bg-indigo-600' to='/signin'>Iniciar Sesión</Link>
+          </li>
+          <li>
+            <ThemeSwitcher />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  // <div>
+  // <header>
+  //     <nav>
+  //       <ul>
+  //       <BsFillMoonStarsFill classNameNameNameNameNameNameNameNameNameName="cursor-pointer" onClick={() => setDarkMode(!darkMode)}/>
+  //         <li><Link to="/">Home</Link></li>
+  //         <li><Link to="/search-page">Search Page</Link></li>
+  //       </ul>
+  //     </nav>
+  //   </header>
+  //   </div>
+  )
 }
-export default Navbar;
+export default Navbar
