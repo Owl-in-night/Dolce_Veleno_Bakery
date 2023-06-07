1ec80f39d244a0dbfd4e97751efa775963d623a5
SANTOS PEDRO BALTAZAR JOJ CANO <santospedrobaltazarjc02@gmail.com>
2023-06-07 17:49:18 -0600

    Veinticincoavo v2 commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ function Home () {
         <img className='grid-item' src='/img/img5.jfif' width={230} height={260} />
         <img className='grid-item' src='/img/img6.jfif' width={230} height={260} />
       </div>
-      <h1 className='text_1 dark:text-white'>Nosotros somos Dolce Veleno Backery un emprendimiento que promueve la producción de postres para toda ocasión y realmente estamos agradecidos por tu presencia en nuestra página, ahora es momento de comprar y adquirir nuestros postres ¡Disfrútalo!</h1>
+      <h1 className='text_1 dark:text-white'>Nosotros somos Dolce Veleno Bakery un emprendimiento que promueve la producción de postres para toda ocasión y realmente estamos agradecidos por tu presencia en nuestra página, ahora es momento de comprar y adquirir nuestros postres ¡Disfrútalo!</h1>
       {/* Spaces for margin manualy */}
       <br />
       <br />
