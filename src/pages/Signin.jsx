35e2b7015bf626150071a04b644f257b1988310f
SANTOS PEDRO BALTAZAR JOJ CANO <santospedrobaltazarjc02@gmail.com>
2023-06-07 17:34:14 -0600

    Veinticuatroavo commit

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useAuth } from '../context/authContext'
 import { Link, useNavigate } from 'react-router-dom'
 import { Alert } from '../components/_partials/Alert'
+import '../App.css'
 
 export function Signin () {
   const [user, setUser] = useState({
@@ -43,26 +44,47 @@ export function Signin () {
     }
   }
   return (
-    <div className='w-full max-w-xs m-auto'>
+    <div className='img_bk'>
       <br />
-      {error && <Alert message={error} />}
-      <form onSubmit={handleSubmit} className='bg-white shadow-md px-8 pt-6 pb-8 mb-4'>
-        <div className='mb-4'>
-          <label htmlFor='email' className='block text-gray-700 text-sm font-bold mg-2'>Email</label>
-          <input type='email' name='email' placeholder='youremail@company.ltd' className='shadow appearance-none border rounder w-full py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={handleChange} />
-        </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className='w-full max-w-xs m-auto'>
+        <br />
+        {error && <Alert message={error} />}
+        <form onSubmit={handleSubmit} className='bg-white shadow-md px-8 pt-6 pb-8 mb-4'>
+          <div className='mb-4'>
+            <label htmlFor='email' className='block text-gray-700 text-sm font-bold mg-2'>Email</label>
+            <input type='email' name='email' placeholder='youremail@company.ltd' className='shadow appearance-none border rounder w-full py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={handleChange} />
+          </div>
 
-        <div className='mb-4'>
-          <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' placeholder='******' className='shadow appearance-none border rounder w-full py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={handleChange} />
-        </div>
-        <div className='flex items-center justify-between'>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Sign In</button>
-          <a href='#!' className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800' onClick={handleResetPassword}>Forgot Password?</a>
-        </div>
-      </form>
-      <p className='my-4 text-sm flex justify-between px-3'> Don't have an account? <Link to='/signup'>Sign Up</Link></p>
-      <button className='bg-slate-50 hover:bg-slate-200 text-black shadow-md rounded border-2 border-gray-300 py-2 px-4 w-full' onClick={handleGoogleSignin}>Sign In with Google</button>
+          <div className='mb-4'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' name='password' id='password' placeholder='******' className='shadow appearance-none border rounder w-full py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={handleChange} />
+          </div>
+          <div className='flex items-center justify-between'>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Sign In</button>
+            <a href='#!' className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800' onClick={handleResetPassword}>Forgot Password?</a>
+          </div>
+        </form>
+        <p className='my-4 text-sm flex justify-between px-3 text-black'> Don't have an account? <Link to='/signup'>Sign Up</Link></p>
+        <button className='bg-slate-50 hover:bg-slate-200 text-black shadow-md rounded border-2 border-gray-300 py-2 px-4 w-full' onClick={handleGoogleSignin}>Sign In with Google</button>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -27,21 +27,44 @@ export function Signup () {
   }
 
   return (
-    <div className='w-full max-w-xs m-auto '>
-      <br />
-      {error && <Alert message={error} />}
-      <form onSubmit={handleSubmit} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-        <div className='mb-4'>
-          <label htmlFor='email' className='block text-gray-700 text-sm font-bold my-2'>Email</label>
-          <input type='email' name='email' placeholder='youremail@company.ltd' className='shadow appearance-none border rounder w-full py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={handleChange} />
-        </div>
-        <div className='mb-4'>
-          <label htmlFor='password' className='block text-gray-700 text-sm font-bold my-2'>Password</label>
-          <input type='password' name='password' id='password' placeholder='******' className='shadow appearance-none border rounder w-full py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={handleChange} />
-        </div>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Sign Up</button>
-      </form>
-      <p className='my-4 text-sm flex justify-between px-3'> Do you have account? <Link to='/signin'>Sign in</Link></p>
+    <div className='img_bk2'>
+      <div className='w-full max-w-xs m-auto'>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        {error && <Alert message={error} />}
+        <form onSubmit={handleSubmit} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+          <div className='mb-4'>
+            <label htmlFor='email' className='block text-gray-700 text-sm font-bold my-2'>Email</label>
+            <input type='email' name='email' placeholder='youremail@company.ltd' className='shadow appearance-none border rounder w-full py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={handleChange} />
+          </div>
+          <div className='mb-4'>
+            <label htmlFor='password' className='block text-gray-700 text-sm font-bold my-2'>Password</label>
+            <input type='password' name='password' id='password' placeholder='******' className='shadow appearance-none border rounder w-full py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' onChange={handleChange} />
+          </div>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Sign Up</button>
+          <p className='my-4 text-sm flex justify-between px-3 text-black'> Do you have account? <Link to='/signin'>Sign in</Link></p>
+        </form>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
     </div>
   )
 }
