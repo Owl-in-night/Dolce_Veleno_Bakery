1991ee8c35b267ebe24a4d138a20fce2701dd4dd
SANTOS PEDRO BALTAZAR JOJ CANO <santospedrobaltazarjc02@gmail.com>
2023-06-08 22:27:43 -0600

    Veinticincoavo v7.2 commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home () {
       <br />
       {/* Buttons */}
       <div className='max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4'>
-        <ul className='flex list-none m-0 p-0 space-x-0 xl:space-x-72'>
+        <ul className='flex list-none m-0 p-0 space-x-0 xl:space-x-72 md:space-x-52'>
           <li>
             <Link to='/contacto' className='inline-flex rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]space py-2 pl-3 pr-4 md:bg-transparent dark:text-white'>Contacto</Link>
           </li>
