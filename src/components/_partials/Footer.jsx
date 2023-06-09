c14f94d57e00d445b0300f8fef2f0b27d8ddf230
SANTOS PEDRO BALTAZAR JOJ CANO <santospedrobaltazarjc02@gmail.com>
2023-06-08 23:48:20 -0600

    Veinticincoavo v7.4 commit

--- src/components/_partials/Footer.jsx
+++ src/components/_partials/Footer.jsx
@@ -4,7 +4,7 @@ function Footer () {
   return (
 
     <footer className='bg-white rounded-lg shadow m-4 my-0 dark:bg-slate-800 border-gray-2001 '>
-      <div className='w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between'>
+      <div className='w-full mx-auto max-w-screen-xl p-4'>
         <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>© 2023 <a href='https://dolcevelenobakery.netlify.app/' className='hover:underline'>Dolce Veleno Bakery™</a>. All Rights Reserved.
         </span>
         <ul className='flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0'>
